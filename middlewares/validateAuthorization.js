a7ea28f63268d93c9ab1bee07abac0ae29cce2f0
Gustavo Dolzan <gudolzan@gmail.com>
2022-02-24 20:11:22 -0300

    Update middlewares

--- middlewares/validateAuthorization.js
+++ middlewares/validateAuthorization.js
@@ -8,11 +8,11 @@ module.exports = rescue(async (req, _res, next) => {
   if (!authorization) throwNewError('tokenNotFound');
 
   // If authorization header is invalid
-  // an error will be thrown by the validate function
+  // an error will be thrown by the jwt.validate function
   // Refer to ../helpers/jwt.js
   const { email } = await jwt.validate(authorization);
 
-  // Add email to request body
+  // Add email to request for later use
   req.email = email;
 
   next();
