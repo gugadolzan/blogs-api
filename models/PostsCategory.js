1d8c52f340a41b35d85e4302fc99b5e0b0d7879f
Gustavo Dolzan <gudolzan@gmail.com>
2022-02-24 19:04:09 -0300

    Update models and migrations

--- models/PostsCategory.js
+++ models/PostsCategory.js
@@ -16,10 +16,7 @@ const associate = (models) => {
 module.exports = (sequelize, _DataTypes) => {
   const PostsCategory = sequelize.define(
     'PostsCategory',
-    {
-      // postId: { type: DataTypes.INTEGER, primaryKey: true },
-      // categoryId: { type: DataTypes.INTEGER, primaryKey: true },
-    },
+    {},
     { timestamps: false, tableName: 'PostsCategories' },
   );
 
